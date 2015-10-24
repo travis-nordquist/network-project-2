c78c743068cfaf5ed7437564512893aed5715896
Kyle Buchmiller <kbuchmil@cs.nmt.edu>
2015-10-24 12:55:08 -0600

    playin

--- src/helloWorld/Hello.java
+++ src/helloWorld/Hello.java
@@ -3,8 +3,7 @@ package helloWorld;
 public class Hello {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.printf("hello world!%nsee?%n");
     }
 
 }
